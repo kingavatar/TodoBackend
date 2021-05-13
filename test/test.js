d8331d3fe018073566a19668e179f5f2cf07b706
sai dhanush <sai@sais-MacBook-Air.local>
2021-05-13 18:32:54 +0530

    just added some testcases. But have to check the return value of passport authentication.

--- test/test.js
+++ test/test.js
@@ -1,23 +1,35 @@
-'use strict';
+// 'use strict';
 const request = require('supertest');
 var mongoose = require('mongoose');
-const {connectDB,connectProductionDB} = require('../config/db')
-//const expect = require("chai").expect;
+const {connectDB} = require('../config/db');
+
+
 // app is supposed to point to the app.js file
-const app = require('../app');
 
-beforeAll(()=>{ 
+const myApp = require('../app');
 
-  connectDB();
+const app = myApp.listen(3000);
+
+beforeAll(async ()=>{ 
+  await connectDB();
+});
+afterAll(async () => { 
+    await mongoose.connection.close()
 });
 
+
+describe('Testing',()=>{
+    it('Testing to see if Jest works', () => {
+        expect(1).toBe(1)
+      })
+})
 
 
 
 describe('Testing POSTS/shots endpoint', function () {
   it('respond with valid HTTP status code and description and message', async ()=> {
       // Make POST Request
-    const response = await request(app).post('/api/auth/login').send({
+    const response = await request(app).post('/api/auth/signin').send({
           email:"saidhanush",
           password:"sai"
       }).expect(404);
@@ -25,11 +37,24 @@ describe('Testing POSTS/shots endpoint', function () {
     });
     it('respond with valid HTTP status code and description and message', async ()=> {
       // Make POST Request
-      const response = await request(app).post('/api/auth/login').send({
+      const response = await request(app).post('/api/auth/signin').send({
           email:"a@a.com",
           password:"12345678"
       }).expect(200);
+    //   console.log(response);
+    //   response.expect(404);
       
     });
+    it('respond with valid HTTP status code and description and message', async ()=> {
+      // Make POST Request
+      const response = await request(app).post('/api/auth/signin').send({
+          email:"purvaj",
+          password:"12345678"
+      }).expect(404);
+    //   console.log(response);
+    //   response.expect(404);
+      
+    });
+
     
 });
