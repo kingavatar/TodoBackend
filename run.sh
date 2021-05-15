2788a4fce79023cd6b25aaa70cedf35b0e3a7af2
kingavatar <saikiranreddy06@gmail.com>
2021-05-15 23:45:21 +0530

    Adding Dynamic IP

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# APP URL
-# frontend build
-# move f build to backend
+
+echo "VUE_APP_URL = $(curl http://169.254.169.254/latest/meta-data/public-ipv4):8000" > /etc/environment
+
+echo "APP_URL = $(curl http://169.254.169.254/latest/meta-data/public-ipv4):8000" > /etc/environment
+
 
 if [ "$ENV" = "PROD" ]
     then
