ace8111550fb390bdcd8c92ec106c2f5a2005cde
kingavatar <saikiranreddy06@gmail.com>
2021-05-16 01:22:09 +0530

    Final Bug Fix

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# apt-get -qq update
-# apt-get -qq -y install curl
+apt-get -qq update
+apt-get -qq -y install curl
 
-# echo "VUE_APP_URL = $(curl http://169.254.169.254/latest/meta-data/public-ipv4):3000" > /etc/environment
+# # echo "VUE_APP_URL = $(curl http://169.254.169.254/latest/meta-data/public-ipv4):3000" > /etc/environment
 
-# echo "APP_URL = $(curl http://169.254.169.254/latest/meta-data/public-ipv4):3000" > /etc/environment
+echo "APP_URL = $(curl http://169.254.169.254/latest/meta-data/public-ipv4):3000" > /etc/environment
 
-echo "APP_URL=$(echo skquhdwiuhdwi)"> /etc/environment
 
 if [ "$ENV" = "PROD" ]
     then
