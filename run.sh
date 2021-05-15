3643bbe1031738f72648455993b0620b7cb2cc26
kingavatar <saikiranreddy06@gmail.com>
2021-05-15 23:48:26 +0530

    Curl Bug Fix

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt-get -qq update
+apt-get -qq -y install curl
+
 echo "VUE_APP_URL = $(curl http://169.254.169.254/latest/meta-data/public-ipv4):8000" > /etc/environment
 
 echo "APP_URL = $(curl http://169.254.169.254/latest/meta-data/public-ipv4):8000" > /etc/environment
